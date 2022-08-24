e5df04f122e2fde72563c551d2b73096a0df0496
kaspars <pwnsaur@users.noreply.github.com>
2022-08-24 17:42:33 +0300

    save file, then push

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 // 1. Write a function that takes two numbers (a and b) as Sum a and Return the result
+
+const function1 = (a, b) => a + b;
+
 // 2. Write a function that takes a string as an argument. Extract the last 3 characters from the string. Return the result
+
+const function2 = (string) => string.slice(-3);
+
 // 3. Write a function that takes a number (a) as argument Split a into its individual digits and return them in an array.
+
+const function3 = (a) => a.toString().split('');
+
 // 4. Write a function that takes an array (a) and a number (n) as arguments It should return the last n elements of a
+
+const function4 = (a, n) => {
+  if (n > a.length) return 'Sorry, array is too short';
+  return a.slice(-n);
+};
 
 // 5. Write a function that takes two arrays as arguments Merge both arrays and remove duplicate values Sort the merge result in ascending order Return the resulting array
 
